e4c13cae80f97e83776276b249984d8f94b6ebe0
MykolaiKot <mykolaikotek1995@gmail.com>
2023-07-10 02:21:58 +0100

    pushtest

--- jamming/src/App.js
+++ jamming/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello
+          Hello my name is Mykolai 
         </a>
       </header>
     </div>
